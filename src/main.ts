68f973db1ac6a7bee375a7f6f000b4646bb9f164
cc1015 <125077096+cc1015@users.noreply.github.com>
2025-02-27 09:37:35 +0900

    fix dependency issue

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import './assets/main.css'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { BootstrapVue3 } from 'bootstrap-vue-3';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App);
-app.use(BootstrapVue3);
 app.mount('#app');
